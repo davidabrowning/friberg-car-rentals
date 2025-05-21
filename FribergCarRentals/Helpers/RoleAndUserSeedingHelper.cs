3bea4278f6c5a695b8ff45088b8a16d9f2017888
David Browning <david.a.browning@gmail.com>
2025-05-21 16:06:33 +0200

    Move role and user seeding functionality to RoleAndUserSeedingHelper helper class.

---
+++ FribergCarRentals/Helpers/RoleAndUserSeedingHelper.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FribergCarRentals.Helpers
+{
+    public static class RoleAndUserSeedingHelper
+    {
+        public static async Task SeedRolesAndDefaultAdminUser(WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                await SeedRoles(scope);
+                await SeedDefaultAdminUser(scope);
+                await SeedAllUsersAsUserRole(scope);
+            }
+        }
+
+        private static async Task SeedRoles(IServiceScope scope)
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roles = { "Admin", "User" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        private static async Task SeedDefaultAdminUser(IServiceScope scope)
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var user = await userManager.FindByEmailAsync("admin@admin.se");
+
+            if (user != null && !(await userManager.IsInRoleAsync(user, "Admin")))
+            {
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+
+        private static async Task SeedAllUsersAsUserRole(IServiceScope scope)
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            List<IdentityUser> allUsers = await userManager.Users.ToListAsync();
+
+            foreach (var user in allUsers)
+            {
+                await userManager.AddToRoleAsync(user, "User");
+            }
+        }
+    }
+}
--- FribergCarRentals/Program.cs
+++ FribergCarRentals/Program.cs
@@ -1,4 +1,5 @@
 using FribergCarRentals.Data;
+using FribergCarRentals.Helpers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,8 +25,7 @@ namespace FribergCarRentals
             var app = builder.Build();
 
             // Seed roles
-            await SeedRolesAndDefaultAdminUser(app);
-
+            await RoleAndUserSeedingHelper.SeedRolesAndDefaultAdminUser(app);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -53,52 +53,6 @@ namespace FribergCarRentals
                .WithStaticAssets();
 
             app.Run();
-        }
-
-        private static async Task SeedRolesAndDefaultAdminUser(WebApplication app)
-        {
-            using (var scope = app.Services.CreateScope())
-            {
-                await SeedRoles(scope);
-                await SeedDefaultAdminUser(scope);
-                await SeedAllUsersAsUserRole(scope);
-            }
-        }
-
-        private static async Task SeedRoles(IServiceScope scope)
-        {
-            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            string[] roles = { "Admin", "User" };
-
-            foreach (var role in roles)
-            {
-                if (!await roleManager.RoleExistsAsync(role))
-                {
-                    await roleManager.CreateAsync(new IdentityRole(role));
-                }
-            }
-        }
-
-        private static async Task SeedDefaultAdminUser(IServiceScope scope)
-        {
-            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-            var user = await userManager.FindByEmailAsync("admin@admin.se");
-
-            if (user != null && !(await userManager.IsInRoleAsync(user, "Admin")))
-            {
-                await userManager.AddToRoleAsync(user, "Admin");
-            }
-        }
-
-        private static async Task SeedAllUsersAsUserRole(IServiceScope scope)
-        {
-            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-            List<IdentityUser> allUsers = await userManager.Users.ToListAsync();
-
-            foreach (var user in allUsers)
-            {
-                await userManager.AddToRoleAsync(user, "User");
-            }
         }
     }
 }
