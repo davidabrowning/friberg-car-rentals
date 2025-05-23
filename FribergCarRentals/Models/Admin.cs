12ffdd55e2b2ed98abf76e4080b5bb7444fd27ee
David Browning <david.a.browning@gmail.com>
2025-05-23 14:21:52 +0200

    Add Admin FirstName and LastName.

--- FribergCarRentals/Models/Admin.cs
+++ FribergCarRentals/Models/Admin.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
     }
 }
