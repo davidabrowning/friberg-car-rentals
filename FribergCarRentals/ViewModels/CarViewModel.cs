30ce890d9a970971ff238b01ab12428e998260ee
David Browning <david.a.browning@gmail.com>
2025-05-23 12:56:56 +0200

    Add CarViewModel.

---
+++ FribergCarRentals/ViewModels/CarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FribergCarRentals.Models;
+
+namespace FribergCarRentals.ViewModels
+{
+    public class CarViewModel
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = "";
+        public string Model { get; set; } = "";
+        public int Year { get; set; } = 0;
+        public string Description { get; set; } = "";
+        List<Reservation> Reservations { get; set; } = new();
+    }
+}
