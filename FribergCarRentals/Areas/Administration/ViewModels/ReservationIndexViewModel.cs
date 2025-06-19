55b888e90eb255392ffe07d67e5189ef60bddfbb
David Browning <david.a.browning@gmail.com>
2025-06-19 19:46:05 +0200

    Remove unnecessary Reservation actions and views.

--- FribergCarRentals/Areas/Administration/ViewModels/ReservationIndexViewModel.cs
+++ FribergCarRentals/Areas/Administration/ViewModels/ReservationIndexViewModel.cs
@@ -7,9 +7,7 @@ namespace FribergCarRentals.Areas.Administration.ViewModels
         public int Id { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
-        public Car? Car { get; set; }
-        public Customer? Customer { get; set; }
-        public List<Car> AllCars { get; set; } = new();
-        public List<Customer> AllCustomers { get; set; } = new();
+        public Car Car { get; set; }
+        public Customer Customer { get; set; }
     }
 }
