df8619b08b64c327bec1d8bac607c0739ec31b86
David Browning <david.a.browning@gmail.com>
2025-11-19 16:14:52 +0100

    Create SampleTests class that simply checks that 1+1=2.

---
+++ FribergCarRentals.Tests/SampleTests.cs
@@ -0,0 +1,12 @@
+﻿namespace FribergCarRentals.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void OnePlusOne_EqualsTwo()
+        {
+            int result = 1 + 1;
+            Assert.Equal(2, result);
+        }
+    }
+}
