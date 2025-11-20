7fbb0f55339fc95a8e56988ee81cc936220c1b83
David Browning <david.a.browning@gmail.com>
2025-11-20 23:24:40 +0100

    Add ReservationsController.

---
+++ FribergCarRentals.Tests/Tests/WebApi/Controllers/ReservationsControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using FribergCarRentals.Core.Models;
+using FribergCarRentals.Tests.Mock.WebApi.Services;
+using FribergCarRentals.WebApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FribergCarRentals.Tests.Tests.WebApi.Controllers
+{
+    public class ReservationsControllerTests
+    {
+        private ReservationsController _reservationController;
+        private Reservation _reservation;
+        public ReservationsControllerTests() {
+            MockReservationService mockReservationService = new();
+            _reservationController = new(mockReservationService);
+            _reservation = new() { Id = 42 };
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsCorrectType()
+        {
+            var result = await _reservationController.Get();
+            Assert.IsType<List<Reservation>>(result);
+        }
+
+        [Fact]
+        public async Task GetOne_ReturnsCorrectTypeAfterPost()
+        {
+            await _reservationController.Post(_reservation);
+            var result = await _reservationController.Get(_reservation.Id);
+            Assert.IsType<Reservation>(result);
+        }
+
+        [Fact]
+        public async Task Post_ReturnsCorrectType()
+        {
+            var result = await _reservationController.Post(_reservation);
+            Assert.IsType<Reservation>(result);
+        }
+
+        [Fact]
+        public async Task Put_UpdatesCarValue()
+        {
+            Car car = new Car() { Id = 88 };
+            await _reservationController.Post(_reservation);
+            _reservation.Car = car;
+            await _reservationController.Put(_reservation.Id, _reservation);
+            Reservation? result = await _reservationController.Get(_reservation.Id);
+            Assert.Equal(car, result.Car);
+        }
+
+        [Fact]
+        public async Task Delete_RemovesReservation()
+        {
+            await _reservationController.Post(new Reservation() { Id = 5 });
+            await _reservationController.Post(new Reservation() { Id = 7 });
+            await _reservationController.Delete(5);
+            IEnumerable<Reservation> result = await _reservationController.Get();
+            result = await _reservationController.Get();
+            Assert.Single(result);
+        }
+    }
+}
---
+++ FribergCarRentals.WebApi/Controllers/ReservationsController.cs
@@ -0,0 +1,57 @@
+﻿using FribergCarRentals.Core.Interfaces.Services;
+using FribergCarRentals.Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FribergCarRentals.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+        public ReservationsController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Reservation>> Get()
+        {
+            IEnumerable<Reservation> reservations = await _reservationService.GetAllAsync();
+            return reservations;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<Reservation?> Get(int id)
+        {
+            Reservation? reservation = await _reservationService.GetByIdAsync(id);
+            return reservation;
+        }
+
+        [HttpPost]
+        public async Task<Reservation> Post(Reservation reservation)
+        {
+            await _reservationService.CreateAsync(reservation);
+            return reservation;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Reservation reservation)
+        {
+            if (id != reservation.Id)
+            {
+                return BadRequest("Id and reservation do not match.");
+            }
+            await _reservationService.UpdateAsync(reservation);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _reservationService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
