7b272208e5afdff0f050a9199ff4becf3d1475b6
David Browning <david.a.browning@gmail.com>
2025-11-21 22:40:30 +0100

    Update CarsController to return CarDtos instead of Cars.

---
+++ FribergCarRentals.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,18 @@
+﻿using FribergCarRentals.Core.Interfaces.Services;
+using FribergCarRentals.WebApi.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FribergCarRentals.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public CustomersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
