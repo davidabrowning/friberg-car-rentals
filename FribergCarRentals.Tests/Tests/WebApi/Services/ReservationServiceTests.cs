476026905004d42caf3c11a31b6dda6c182f97d8
David Browning <david.a.browning@gmail.com>
2025-11-21 19:11:07 +0100

    Simplify ReservationServiceTests.

--- FribergCarRentals.Tests/Tests/WebApi/Services/ReservationServiceTests.cs
+++ FribergCarRentals.Tests/Tests/WebApi/Services/ReservationServiceTests.cs
@@ -9,44 +9,38 @@ namespace FribergCarRentals.Tests.Tests.WebApi.Services
 {
     public class ReservationServiceTests
     {
-        // Reused variables
-        private IReservationService reservationService;
-        private IEnumerable<Reservation>? reservations;
-        private Car? car;
-        private Reservation? reservation;
+        private IReservationService _reservationService;
 
         public ReservationServiceTests()
         {
-            IRepository<Reservation> reservationRepository = new MockReservationRepository();
-            reservationService = new ReservationServiceSeparated(reservationRepository);
+            MockReservationRepository reservationRepository = new();
+            _reservationService = new ReservationServiceSeparated(reservationRepository);
         }
 
         [Fact]
         public async Task GetByCarAsync_ShouldReturnEmptyListIfNewCar()
         {
-            car = new();
-            reservations = await reservationService.GetByCarAsync(car);
+            Car car = new();
+            IEnumerable<Reservation> reservations = await _reservationService.GetByCarAsync(car);
             Assert.Empty(reservations);
         }
 
         [Fact]
         public async Task GetByCarAsync_ShouldReturnListOfOneIfCarHasBeenAdded()
         {
-            car = new();
-            reservation = new() { Car = car };
-            await reservationService.CreateAsync(reservation);
-            reservations = await reservationService.GetByCarAsync(car);
+            Car car = new();
+            Reservation reservation = new() { Car = car };
+            await _reservationService.CreateAsync(reservation);
+            IEnumerable<Reservation> reservations = await _reservationService.GetByCarAsync(car);
             Assert.Single(reservations);
         }
 
         [Fact]
-        public async Task GetByCarAsync_ShouldReturnEmptyIfTestedAgainWithoutReaddingCar()
+        public async Task GetByCarAsync_ShouldReturnEmptyIfCarIsNotAdded()
         {
-            car = new();
-            reservation = new() { Car = car };
-            // await reservationService.CreateAsync(reservation); <== tests that this line is required
-            //                                                        to re-add reservation
-            reservations = await reservationService.GetByCarAsync(car);
+            Car car = new();
+            Reservation reservation = new() { Car = car };
+            IEnumerable<Reservation> reservations = await _reservationService.GetByCarAsync(car);
             Assert.Empty(reservations);
         }
     }
