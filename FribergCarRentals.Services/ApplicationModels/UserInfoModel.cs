5b07f11df12ad78df08411cae8c9abebcf3c5117
David Browning <david.a.browning@gmail.com>
2025-11-28 20:13:37 +0100

    Create UserInfoModel.

---
+++ FribergCarRentals.Services/ApplicationModels/UserInfoModel.cs
@@ -0,0 +1,13 @@
+﻿using FribergCarRentals.Core.Models;
+
+namespace FribergCarRentals.Services.ApplicationModels
+{
+    internal class UserInfoModel
+    {
+        public required string UserId;
+        public required string Username;
+        public List<string> AuthRoles = new();
+        public Admin? Admin;
+        public Customer? Customer;
+    }
+}
