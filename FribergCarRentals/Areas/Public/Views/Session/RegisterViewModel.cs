d30c58093dcaf7226ab0d9a88ab87b45d0d02269
David Browning <david.a.browning@gmail.com>
2025-11-28 21:55:54 +0100

    Split Signin into separate Signin and Register methods. Invert negative if conditions back to positive. Remove unused usings.

---
+++ FribergCarRentals/Areas/Public/Views/Session/RegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FribergCarRentals.Mvc.Areas.Public.Views.Session
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
