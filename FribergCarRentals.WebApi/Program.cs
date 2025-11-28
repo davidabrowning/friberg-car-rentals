b786060f769011dd2b809759d3169de0a93e8437
David Browning <david.a.browning@gmail.com>
2025-11-28 19:49:21 +0100

    Delete Facades folder from WebApi project. Clean up Program.cs.

--- FribergCarRentals.WebApi/Program.cs
+++ FribergCarRentals.WebApi/Program.cs
@@ -5,8 +5,8 @@ using FribergCarRentals.Core.Interfaces.Repositories;
 using FribergCarRentals.Core.Interfaces.Services;
 using FribergCarRentals.Core.Models;
 using FribergCarRentals.Data;
+using FribergCarRentals.Services.Facades;
 using FribergCarRentals.Services.Services;
-using FribergCarRentals.WebApi.Facades;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -16,86 +16,133 @@ namespace FribergCarRentals.WebApi
 {
     public class Program
     {
+        private static WebApplicationBuilder? builder;
+        private static WebApplication? app;
         public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder = WebApplication.CreateBuilder(args);
+            PrepDependencyInjection();
+            app = builder.Build();
+            await PrepDatabase();
+            ConfigureMiddleware();
+            ConfigureEndpoints();
+            app.Run();
+        }
 
-            // Add services to the container.
+        private static void PrepDependencyInjection()
+        {
+            AddDatabase();
+            AddRepository();
+            AddServices();
+            AddIdentity();
+            AddJwt();
+            AddControllers();
+            AddOpenApi();
+        }
+
+        private static void AddDatabase()
+        {
+            string connectionString = builder!.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-            builder.Services.AddScoped<IRepository<Admin>, AdminRepository>();
+            builder.Services.AddScoped<IDatabaseCleaner, DatabaseCleaningService>();
+            builder.Services.AddScoped<IDatabaseSeeder, DatabaseSeedingService>();
+        }
+
+        private static void AddRepository()
+        {
+            builder!.Services.AddScoped<IRepository<Admin>, AdminRepository>();
             builder.Services.AddScoped<IRepository<Car>, CarRepository>();
             builder.Services.AddScoped<IRepository<Customer>, CustomerRepository>();
             builder.Services.AddScoped<IRepository<Reservation>, ReservationRepository>();
-            builder.Services.AddScoped<IAdminService, AdminService>();
+        }
+
+        private static void AddServices()
+        {
+            builder!.Services.AddScoped<IAdminService, AdminService>();
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddScoped<ICarService, CarService>();
             builder.Services.AddScoped<ICustomerService, CustomerService>();
             builder.Services.AddScoped<IReservationService, ReservationService>();
             builder.Services.AddScoped<IApplicationFacade, ApplicationFacade>();
-            builder.Services.AddScoped<IDatabaseCleaner, DatabaseCleaningService>();
-            builder.Services.AddScoped<IDatabaseSeeder, DatabaseSeedingService>();
+        }
 
-            // Identity login
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+        private static void AddIdentity()
+        {
+            builder!.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
 
-            // JWT login
-            builder.Services.AddAuthentication("JwtBearer")
+        private static void AddJwt()
+        {
+            builder!.Services.AddAuthentication("JwtBearer")
                 .AddJwtBearer("JwtBearer", options =>
-                {
-                    options.TokenValidationParameters = new()
                     {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidateLifetime = true,
-                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                        ValidAudience = builder.Configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:KEy"]!)
-                            )
-                    };
-                });
+                        options.TokenValidationParameters = new()
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidateLifetime = true,
+                            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                            ValidAudience = builder.Configuration["Jwt:Audience"],
+                            IssuerSigningKey = new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:KEy"]!)
+                                )
+                        };
+                    });
             builder.Services.AddScoped<IJwtService, JwtService>();
+        }
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+        private static void AddControllers()
+        {
+            builder!.Services.AddControllers();
+        }
 
-            var app = builder.Build();
+        private static void AddOpenApi()
+        {
+            builder!.Services.AddOpenApi();
+        }
 
-            // Seed roles, default admin user, and default cars
+        private async static Task PrepDatabase()
+        {
+            await SeedDatabase();
+            await CleanDatabase();
+        }
+
+        private async static Task SeedDatabase()
+        {
             using (IServiceScope scope = app.Services.CreateScope())
             {
                 IDatabaseSeeder seedingService = scope.ServiceProvider.GetRequiredService<IDatabaseSeeder>();
                 await seedingService.SeedAsync();
             }
+        }
 
-            // Run DB cleanup
+        private async static Task CleanDatabase()
+        {
             using (IServiceScope scope = app.Services.CreateScope())
             {
                 IDatabaseCleaner cleaningService = scope.ServiceProvider.GetRequiredService<IDatabaseCleaner>();
                 await cleaningService.CleanAsync();
             }
+        }
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+        private static void ConfigureMiddleware()
+        {
+            if (app!.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
                 app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "FribergCarRentalsApi"));
             }
-
             app.UseHttpsRedirection();
-
             app.UseAuthentication();
             app.UseAuthorization();
+        }
 
-
-            app.MapControllers();
-
-            app.Run();
+        private static void ConfigureEndpoints()
+        {
+            app!.MapControllers();
         }
     }
 }
