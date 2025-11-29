be9677e4748eedbbf40a4fdf690cacee7994b1f2
David Browning <david.a.browning@gmail.com>
2025-11-29 20:11:49 +0100

    Delete MockDatabase. Replace with List<Reservation> in MockReservationRepository.

--- FribergCarRentals.Tests/Mock/Data/Repositories/MockReservationRepository.cs
+++ FribergCarRentals.Tests/Mock/Data/Repositories/MockReservationRepository.cs
@@ -1,14 +1,14 @@
 ﻿using FribergCarRentals.Core.Interfaces.Repositories;
 using FribergCarRentals.Core.Models;
-using FribergCarRentals.Tests.Mock.Data.Database;
 
 namespace FribergCarRentals.Tests.Mock.Data.Repositories
 {
     public class MockReservationRepository : IRepository<Reservation>
     {
+        private List<Reservation> _reservations = new();
         public Task AddAsync(Reservation reservation)
         {
-            MockDatabase.Reservations.Add(reservation);
+            _reservations.Add(reservation);
             return Task.CompletedTask;
         }
 
@@ -19,7 +19,7 @@ namespace FribergCarRentals.Tests.Mock.Data.Repositories
 
         public Task<IEnumerable<Reservation>> GetAllAsync()
         {
-            IEnumerable<Reservation> reservations = MockDatabase.Reservations;
+            IEnumerable<Reservation> reservations = _reservations;
             return Task.FromResult(reservations);
         }
 
